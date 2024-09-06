8d2503d8f5266e4cb7966e975f8abd191cbedcf1
rimsha_shehzadi <rimshashehzadi79@gmail.com>
2024-09-06 13:40:36 +0500

    URL dispatcher

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 
-from . import views
+from polls import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.index, name="polls"),
+    path("about", views.about, name="about"),
+    path("services", views.services, name="services"),
+    path ("contact", views.contact,name='contact'),
+   
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,8 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse      # render is use for render the template in the project 
+#HttpRespone is a class in django that returns http respone 
 
 # Create your views here.
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("this is polls page")
+
+def about(request):
+    return HttpResponse("this is about page")
+
+def services(request):
+    return HttpResponse("this is services services")
+
+def contact(request):
+    return HttpResponse("this is contact page")
+
